f5acc2d4c54c815928c38cf6919014e16d4abf07
prithvirajhi <prithvirajhi@gmail.com>
2016-03-21 00:53:57 -0500

    new simple logs

---
+++ new_script/script/new3.R
@@ -0,0 +1,7 @@
+#gaps_br<-c(7, 7.5, 7.5, 4.5, 9)
+gaps_flow<-c(2.5, 4, 3.5, 4.5, 1.5, 2, 3.5, 4, 3, 2, 2, 5.5, 2.5, 4, 3)
+#gaps<-rbind(gaps_br, gaps_flow)
+gaps<-rbind(gaps_flow)
+pdf(file="/home/prithvirajhi/new_thesis/Thesis/new_script/R/Simpleduration.pdf", width=8, height=3.5)
+barplot(gaps, xlab="Experiment", ylab="Handoff duration (s)", col=c("blue"), beside=TRUE, ylim=c(0, 8), names.arg=c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"), legend.text=c("ÆtherFlow configuration without prediction"), args.legend=list(x="topright", cex=0.5))
+dev.off()
